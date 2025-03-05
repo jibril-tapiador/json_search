cb2608f6e6c88a5582394e1b3f33044ae5ddc443
Jibril Tapiador <tapiador@jib.is>
2025-03-06 04:03:32 +0800

    initial

---
+++ spec/json_search_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JsonSearch do
+  it "has a version number" do
+    expect(JsonSearch::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
