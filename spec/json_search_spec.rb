6b98345c5c56e655336e12bf73813dc42acac0ec
Jibril Tapiador <tapiador@jib.is>
2025-03-07 04:01:28 +0800

    test: update basic version check test

--- spec/json_search_spec.rb
+++ spec/json_search_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe JsonSearch do
-  it "has a version number" do
+  it 'has a version number' do
     expect(JsonSearch::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
