21b05d20840b39cfdfdc36ca109d6096edd73ffe
Jibril Tapiador <tapiador@jib.is>
2025-03-07 04:24:09 +0800

    refactor: streamline module loading in the main library file

--- lib/json_search.rb
+++ lib/json_search.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
-require_relative "json_search/version"
+require_relative 'json_search/version'
+require_relative 'json_search/record'
+require_relative 'json_search/record_repository'
+require_relative 'json_search/search_service'
+require_relative 'json_search/duplicate_checker'
+require_relative 'json_search/cli'
 
 module JsonSearch
   class Error < StandardError; end
